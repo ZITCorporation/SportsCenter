d31600ed7ac8cfe6b00647d435c9a831e3a1795c
Szyoo <losiner.y@gmail.com>
2022-10-26 12:44:26 +0900

    sync

--- src/main/java/jp/co/sss/sportsCenter/form/ReserveForm.java
+++ src/main/java/jp/co/sss/sportsCenter/form/ReserveForm.java
@@ -1,9 +1,67 @@
 package jp.co.sss.sportsCenter.form;
 
-import org.springframework.stereotype.Controller;
-
-@Controller
 public class ReserveForm {
-    // private String email;
-    // private String password;
+    private int reserveManagementId;
+    private int userId;
+    private int facilityId;
+    private String startTime;
+    private String endingTime;
+    private int toolId;
+    private int toolNum;
+
+    public int getReserveManagementId() {
+        return reserveManagementId;
+    }
+
+    public void setReserveManagementId(int reserveManagementId) {
+        this.reserveManagementId = reserveManagementId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getFacilityId() {
+        return facilityId;
+    }
+
+    public void setFacilityId(int facilityId) {
+        this.facilityId = facilityId;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndingTime() {
+        return endingTime;
+    }
+
+    public void setEndingTime(String endingTime) {
+        this.endingTime = endingTime;
+    }
+
+    public int getToolId() {
+        return toolId;
+    }
+
+    public void setToolId(int toolId) {
+        this.toolId = toolId;
+    }
+
+    public int getToolNum() {
+        return toolNum;
+    }
+
+    public void setToolNum(int toolNum) {
+        this.toolNum = toolNum;
+    }
 }
